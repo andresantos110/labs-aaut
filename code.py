d4e334b1490a6c9ccd8c1a077627f480e8992956
André Santos <106824104+andresantos110@users.noreply.github.com>
2023-09-23 11:41:19 +0100

    alguns fixes, todo mantem-se

--- code.py
+++ code.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from sklearn import datasets, linear_model
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn import linear_model
+from sklearn.model_selection import  KFold
 
-X_test = np.load('X_test_regression1.npy')
-X_train = np.load('X_train_regression1.npy') 
-Y_train = np.load('y_train_regression1.npy')
+Xtest = np.load('X_test_regression1.npy')
+Xtrain = np.load('X_train_regression1.npy') 
+Ytrain = np.load('y_train_regression1.npy')
 
 SSE_linear = 0
 SSE_ridge = 0
@@ -15,29 +15,32 @@ SSE_lasso = 0
 for i in range(15):
     
     kf = KFold(n_splits=5, shuffle=True, random_state=i)
-    N_splits = kf.get_n_splits(X_train)
+    N_splits = kf.get_n_splits(Xtrain)
         
-    for train_index, test_index in kf.split(X_train):
+    for train_index, test_index in kf.split(Xtrain):
+        
+        X_train, X_test = Xtrain[train_index], Xtrain[test_index]
+        Y_train, Y_test = Ytrain[train_index], Ytrain[test_index]
         
         # Linear Regression
         linReg = linear_model.LinearRegression()
         linReg.fit(X_train, Y_train)
         Y_pred_linReg = linReg.predict(X_test)
-        Y_pred_linReg_c = np.reshape(Y_pred_linReg, (int(100/N_splits),1))
+        Y_pred_linReg_c = np.reshape(Y_pred_linReg, (int(15/N_splits),1))
         # temos vetor coluna com valores de Y, calcular SSE e somar a total, no fim calcular media
         
         # Ridge Regression
         ridReg = linear_model.Ridge()
         ridReg.fit(X_train, Y_train)
         Y_pred_ridReg = ridReg.predict(X_test)
-        Y_pred_linReg_c = np.reshape(Y_pred_ridReg, (int(100/N_splits),1))
+        Y_pred_linReg_c = np.reshape(Y_pred_ridReg, (int(15/N_splits),1))
         # temos vetor coluna com valores de Y, calcular SSE e somar a total, no fim calcular media
         
         # Lasso Regression
         lasReg = linear_model.Lasso()
         lasReg.fit(X_train, Y_train) # not sure se falta aqui alguma coisa
         Y_pred_lasReg = lasReg.predict(X_test)
-        Y_pred_lasReg_c = np.reshape(Y_pred_lasReg, (int(100/N_splits),1))
+        Y_pred_lasReg_c = np.reshape(Y_pred_lasReg, (int(15/N_splits),1))
         # temos vetor coluna com valores de Y, calcular SSE e somar a total, no fim calcular media
         
         #TODO
@@ -46,10 +49,3 @@ for i in range(15):
             
 
 #para remover (?)
-
-# The coefficients
-print("Coefficients: \n", regr.coef_)
-# The mean squared error
-print("Mean squared error: %.2f" % mean_squared_error(Y_train, Y_pred[0:15]))
-# The coefficient of determination: 1 is perfect prediction
-print("Coefficient of determination: %.2f" % r2_score(Y_train, Y_pred[0:15]))
