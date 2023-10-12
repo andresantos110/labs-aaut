5d98d34bb1d53c6c15a05ffd8e321e2a982427b3
nuno.j.santos <nunosantos2001@hotmail.com>
2023-10-12 13:01:17 +0100

    added P3 folder and start of p3.py 3

---
+++ P3/p3.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+Xtest = np.load('Xtest_Classification1.npy')
+Xtrain = np.load('Xtrain_Classification1.npy') 
+Ytrain = np.load('ytrain_Classification1.npy')
