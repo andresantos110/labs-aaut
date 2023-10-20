0bf0d2e899288d22082592e5a8983896c26ed576
André Santos <106824104+andresantos110@users.noreply.github.com>
2023-10-20 13:57:04 +0100

    Update p3_data_aug_new.py

--- P3/p3_data_aug_new.py
+++ P3/p3_data_aug_new.py
@@ -5,50 +5,22 @@ from sklearn.model_selection import  train_test_split
 import tensorflow as tf
 from tensorflow.keras import layers, models
 from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.regularizers import l2
-from sklearn.metrics import balanced_accuracy_score
-
-tf.compat.v1.enable_eager_execution()
-
-# =============================================================================
-# def balanced_accuracy(y_true, y_pred):
-#     y_pred_binary = tf.round(y_pred)
-#     true_positives = tf.reduce_sum(y_true * y_pred_binary)
-#     actual_positives = tf.reduce_sum(y_true)
-#     predicted_positives = tf.reduce_sum(y_pred_binary)
-#     balanced_acc = (true_positives / actual_positives + predicted_positives) / 2
-#     return balanced_acc
-# =============================================================================
-
 
 def balanced_accuracy(y_true, y_pred):
-    y_true_np = y_true.numpy()
-    y_pred_np = y_pred.numpy()
-      
-    balanced_acc = balanced_accuracy_score(y_true_np, y_pred_np)
+    y_true = tf.cast(y_true, dtype=tf.float32)
+    y_pred = tf.cast(y_pred, dtype=tf.float32)
+
+    true_positives = tf.reduce_sum(y_true * y_pred)
+    true_negatives = tf.reduce_sum((1 - y_true) * (1 - y_pred))
+    false_positives = tf.reduce_sum((1 - y_true) * y_pred)
+    false_negatives = tf.reduce_sum(y_true * (1 - y_pred))
+
+    sensitivity = true_positives / (true_positives + false_negatives + 1e-7)
+    specificity = true_negatives / (true_negatives + false_positives + 1e-7)
+
+    balanced_acc = (sensitivity + specificity) / 2.0
+
     return balanced_acc
-    
-    
-# =============================================================================
-# def data_augmentation(image, label):
-#     print("running data aug.")
-#     random_num = random.randint(1,4)
-#     print(random_num)
-#     if random_num == 1:
-#         image = tf.image.random_flip_left_right(image)
-#     elif random_num == 2:
-#         image = tf.image.random_flip_up_down(image)
-#     elif random_num == 3:
-#         image = tf.image.rot90(image)
-#     elif random_num == 4:
-#         image = tf.roll(image, shift=[1, 1], axis=[0, 1])
-#     
-#     #image = tf.roll(image, shift=[1, 1], axis=[0, 1])
-#     #image = tf.image.rot90(image)
-#     #image = tf.image.random_flip_up_down(image)
-#     #image = tf.image.random_flip_left_right(image)                                       
-#     return image, label
-# =============================================================================
 
 #CHECK FOR GPU
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -82,31 +54,9 @@ count_ones = np.count_nonzero(Y_train == 1)
 Y_train = tf.keras.utils.to_categorical(Y_train, num_classes = 2)
 Y_test = tf.keras.utils.to_categorical(Y_test, num_classes = 2)
 
-row = np.asmatrix([[0,1]])
-
-#DATA AUGMENTATION mau?
-# =============================================================================
-# dataset = tf.data.Dataset.from_tensor_slices((X_train, Y_train))
-# 
-# majority_dataset = dataset.filter(lambda feature, label: label == 0)
-# minority_dataset = dataset.filter(lambda feature, label: label == 1)
-# 
-# num_minority_samples = len(list(minority_dataset))
-# num_majority_samples = len(list(majority_dataset))
-# 
-# minority_dataset = minority_dataset.repeat(count=num_majority_samples // num_minority_samples)
-# 
-# minority_dataset = minority_dataset.map(data_augmentation)
-# 
-# augmented_dataset = tf.data.experimental.sample_from_datasets([majority_dataset, minority_dataset])
-# 
-# #augmented_dataset = augmented_dataset.map(data_augmentation)
-# 
-# X_train = np.array([data[0] for data in augmented_dataset])
-# Y_train = np.array([data[1] for data in augmented_dataset])
-# =============================================================================
-
 #DATA AUGMENTATION
+
+row = np.asmatrix([[0,1]])
 
 diff = count_zeros - count_ones
 initdiff = diff
@@ -118,21 +68,22 @@ while diff != 0:
     
     if(Y_train[random_number,1] == 1):
         operation = random.randint(1,4)
-        #print(operation)
         if operation == 1:
-            image = tf.image.random_flip_left_right(X_train[random_number])
+            image = tf.image.flip_left_right(X_train[random_number])
         elif operation == 2:
-            image = tf.image.random_flip_up_down(X_train[random_number])
+            image = tf.image.flip_up_down(X_train[random_number])
         elif operation == 3:
             image = tf.image.rot90(X_train[random_number])
         elif operation == 4:
             image = tf.roll(X_train[random_number], shift=[-1, 1], axis=[0, 1])
         diff -= 1
         
-        #image2 = tf.io.encode_png(X_train[random_number], compression=-1, name=None)
+        #print(operation)
         #plt.imshow(X_train[random_number])
+        #plt.xlabel(operation)
         #plt.show()
         #plt.imshow(image)
+        #plt.xlabel(operation)
         #plt.show()
         
         
@@ -152,7 +103,6 @@ model.add(layers.MaxPooling2D((2, 2)))
 #model.add(layers.Conv2D(32, (3, 3), activation='relu'))
 model.add(layers.Dropout(0.2))
 model.add(layers.Flatten())
-#model.add(layers.Dense(32, activation='relu',kernel_regularizer=l2(0.1)))
 model.add(layers.Dense(32, activation='relu'))
 model.add(layers.Dense(2, activation='softmax'))
 
@@ -162,20 +112,15 @@ model.compile(optimizer='adam',
 
 model.summary()
 
-early_stopping = EarlyStopping(monitor='balanced_accuracy', mode='max', patience=20)
+early_stopping = EarlyStopping(monitor='balanced_accuracy', mode='max', patience=10)
 
 epochs = 200
-
-#class_0_weight = total_samples / (2 * 0.84 * total_samples)
-#class_1_weight = total_samples / (2 * 0.16 * total_samples)
-
-#class_weights = {0: class_0_weight, 1: class_1_weight}
 
 history = model.fit(X_train, Y_train, epochs = epochs, validation_data = (X_test,Y_test), callbacks = [early_stopping])
 
 #PLOT ACCURACY
-plt.plot(history.history['balanced_accuracy_score'], label='balanced_accuracy_score')
-plt.plot(history.history['val_balanced_accuracy_score'], label = 'val_balanced_accuracy_score')
+plt.plot(history.history['balanced_accuracy'], label='balanced_accuracy')
+plt.plot(history.history['val_balanced_accuracy'], label = 'val_balanced_accuracy')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.ylim([0.5, 1])
